0dd18cf3e983c6f6fb33090384f407eff3b06ac2
robmarkcole <robmarkcole@gmail.com>
2018-02-18 17:26:43 +0000

    Update tests

--- tests/test_filesize.py
+++ tests/test_filesize.py
@@ -1,23 +1,20 @@
 """The tests for the filesize sensor."""
 import unittest
-from unittest.mock import Mock, patch
+import os
 
 from homeassistant.components.sensor.filesize import CONF_FILE_PATHS
 from homeassistant.setup import setup_component
-from tests.common import get_test_home_assistant, mock_registry
-
-PATH = '/Users/robincole/.homeassistant/home-assistant_v2.db'
+from tests.common import get_test_home_assistant
 
 
-class FakeObj():
-    '''Fake object for testing.'''
-    def __init__(self, stat, mtime, st_mode):
-        self.st_size = stat
-        self.st_mtime = mtime
-        self.st_mode = st_mode
+TEST_DIR = os.path.join(os.path.dirname(__file__))
+TEST_FILE = os.path.join(TEST_DIR, 'mock_file_test_filesize.txt')
 
 
-FAKE = FakeObj(37990000, 1518126363.5514238, 33188)
+def create_file(path):
+    """Create a test file."""
+    with open(path, 'w') as test_file:
+        test_file.write("test")
 
 
 class TestFileSensor(unittest.TestCase):
@@ -26,30 +23,36 @@ class TestFileSensor(unittest.TestCase):
     def setup_method(self, method):
         """Set up things to be run when tests are started."""
         self.hass = get_test_home_assistant()
-        mock_registry(self.hass)
+        self.hass.config.whitelist_external_dirs = set((TEST_DIR))
 
     def teardown_method(self, method):
         """Stop everything that was started."""
+        if os.path.isfile(TEST_FILE):
+            os.remove(TEST_FILE)
         self.hass.stop()
 
-    @patch('os.path.isfile', Mock(return_value=True))
-    @patch('os.access', Mock(return_value=True))
-    @patch('os.stat', Mock(return_value=FAKE))
-    def test_filesize_class(self):
-
+    def test_invalid_path(self):
+        """Test that an invalid path is caught."""
         config = {
             'sensor': {
                 'platform': 'filesize',
-                CONF_FILE_PATHS: [PATH]}
+                CONF_FILE_PATHS: ['invalid_path']}
         }
-
         self.assertTrue(
             setup_component(self.hass, 'sensor', config))
+        assert len(self.hass.states.entity_ids()) == 0
 
-        state = self.hass.states.get('sensor.homeassistant_v2db')
-
-        assert state.state == 37.99
-        assert state.attributes.get('bytes') == 37990000
-        assert state.attributes.get(
-            'last_updated') == '2018-02-08 21:46:03.551424'
-        assert state.attributes.get('path') == PATH
+    def test_valid_path(self):
+        """Test for a valid path."""
+        create_file(TEST_FILE)
+        config = {
+            'sensor': {
+                'platform': 'filesize',
+                CONF_FILE_PATHS: [TEST_FILE]}
+        }
+        self.assertTrue(
+            setup_component(self.hass, 'sensor', config))
+        assert len(self.hass.states.entity_ids()) == 1
+        state = self.hass.states.get('sensor.mock_file_test_filesizetxt')
+        assert state.state == '0.0'
+        assert state.attributes.get('bytes') == 4
